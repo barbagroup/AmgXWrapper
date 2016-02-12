05eeb29450a6c7cb863ffe8da6d6a64fdf2ec0d4
Pi-Yueh Chuang <pychuang@gwu.edu>
2016-02-12 15:54:20 -0500

    New way to handle all-Neumann BC in matrix A
    Previousy we modified A[0][0] and rhs[0] in order to change the
    indefinite issue of the all-Neumann BC system. Now we use a new way: we
    let A[0][0] = 1 and A[0][*] = A[*][0] = 0; and the rhs vector is
    modified correspondingly.
    
    
    Former-commit-id: dec56695d361b03c00ea1c1f453319014dc308df

--- tests/Poisson/src/headers.hpp
+++ tests/Poisson/src/headers.hpp
@@ -65,3 +65,5 @@ PetscErrorCode solve(KSP &ksp, Mat &A, Vec &u, Vec &rhs, Vec &u_exact, Vec &err,
 
 PetscErrorCode solve(AmgXSolver &amgx, Mat &A, Vec &u, Vec &rhs, Vec &u_exact, Vec &err,
         StructArgs &args, PetscLogEvent &warmUpEvent, PetscLogEvent &solvingEvent);
+
+PetscErrorCode applyNeumannBC(Mat &A, Vec &RHS, const Vec &exact);
