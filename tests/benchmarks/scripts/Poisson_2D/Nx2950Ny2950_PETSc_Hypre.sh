ef10007f54a2c0e92e40e1b31bb9734d9c614233
Pi-Yueh <pychuang@gwu.edu>
2016-02-14 01:20:44 -0500

    Add shell scripts for launching benchmarks
    Former-commit-id: a1d99f5198c9f33998951d31755f69922e670f19

---
+++ tests/benchmarks/scripts/Poisson_2D/Nx2950Ny2950_PETSc_Hypre.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+
+#======================================================================
+# PETSc + Hypre; Nx = 2950; Ny = 2950;
+#======================================================================
+PART=short
+TIME=00:05:00
+TARGET=${PWD}/scripts/launchPoisson.sh
+MODE=PETSc
+ALGORITHM=Hypre
+
+launchSbatch 1 16 ${TIME} ${PART} "${TARGET} ${MODE} ${ALGORITHM} 16 16 2950 2950 0"
+launchSbatch 2 16 ${TIME} ${PART} "${TARGET} ${MODE} ${ALGORITHM} 32 16 2950 2950 0"
+launchSbatch 4 16 ${TIME} ${PART} "${TARGET} ${MODE} ${ALGORITHM} 64 16 2950 2950 0"
+launchSbatch 8 16 ${TIME} ${PART} "${TARGET} ${MODE} ${ALGORITHM} 128 16 2950 2950 0"
+launchSbatch 16 16 ${TIME} ${PART} "${TARGET} ${MODE} ${ALGORITHM} 256 16 2950 2950 0"
+
