b15215da0091fe9a14e2ed1510cd4ee7c5b2518a
Pi-Yueh Chuang <pychuang@gwu.edu>
2017-06-01 15:55:28 -0400

    reformat applyNeumannBC; change the diagonal value

---
+++ example/poisson/src/applyNeumannBC.hpp
@@ -0,0 +1,33 @@
+/**
+ * \file applyNeumannBC.hpp
+ * \brief prototypes of applyNeumannBC.
+ * \author Pi-Yueh Chuang (pychuang@gwu.edu)
+ * \date 2017-06-01
+ */
+
+
+# pragma once
+
+// PETSc
+# include <petscsys.h>
+# include <petscmat.h>
+# include <petscvec.h>
+
+
+/**
+ * \brief modify matrix A and RHS vector for all-Neumann BCs
+ *
+ * All-Neumann BC causes issues like indefinite matrix or infinite number of 
+ * solutions. We hence assume the exact solution at the node i = j = 0 is known.
+ * (The same way we deal with the pressure in CFD applications.) The entry 
+ * A[0, 0] is modified to a value close to other diagonal coefficients, and
+ * A[0, *] = A[*, 0] = 0; also all corresponding entries in RHS vector is
+ * modified respectively.
+ *
+ * \param A [in, out] matrix A generated from the function generateA(...)
+ * \param RHS [in, out] the right-hand-side vector generated from generateRHS(...)
+ * \param exact [in] the exact solution vector generated from generateExt(...)
+ *
+ * \return PetscErrorCode.
+ */
+PetscErrorCode applyNeumannBC(Mat &A, Vec &RHS, const Vec &exact);
